9831c5fa13e09adf0e59bea36e04fc68f9c77e8d
Jimmy Satterfield <fedora.game.dev@gmail.com>
2019-03-11 15:17:52 -0700

    Groundwork for layout generation started

--- main.js
+++ main.js
@@ -1,3 +1,42 @@
-module.exports = (name = "User") => {
-    return `Hello ${name}!`;
+let Files = {};
+
+const SplitType = {
+    HORIZONTAL: 1,
+    VERTICAL: 2
 }
+
+class Layout {
+    constructor(layout) {
+        this.layout = layout;
+    }
+
+    static GenerateLayout(app, html) {
+        //Magic here!
+        //returns each window/tabbed window in an overall layout with files defined in Files loaded
+        //elements should have events bound automagically
+        //
+    }
+}
+
+class SplitLayout extends Layout {
+    constructor(splitType, layoutA, layoutB) {
+        this.splitType = splitType;
+        this.layout = layoutA;
+        this.layoutB = layoutB;
+    }
+}
+
+class Window extends Layout {
+    constructor(contents) {
+        this.contents = contents;
+    }
+}
+
+class TabbedWindow extends Window {
+    constructor(tabs, selectedTab) {
+        this.tabs = tabs;
+        this.selectedTab = selectedTab;
+    }
+}
+
+module.exports = { Files, Layout };
