173a8186340bb85a9ebf1b5566922cb41d5c1bcb
DaltonHart <DaltonVaren@gmail.com>
2019-03-13 19:25:55 -0700

    layout class successfully calls split as method

--- example-project/main.js
+++ example-project/main.js
@@ -9,8 +9,18 @@ app.on("ready", () =>
 {
     let mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
-    layout = new Layout(
-        // new SplitLayout(SplitType.HORIZONTAL, 25,
+    layout = new Layout();
+    layout.updateStyles(styles)
+    layout.setLayout(SplitType.VERTICAL, 25, new Window("Test File 3.html"), new Window("Test File 1.html"))
+
+    let layoutHtml = layout.GenerateHtml(rootPath);
+
+    mainWindow.loadURL(`data:text/html;charset=utf-8,${layoutHtml}`);
+});
+
+//holding for reference
+
+// new SplitLayout(SplitType.HORIZONTAL, 25,
         //     // new SplitLayout(SplitType.VERTICAL, 25,
         //     //     new Window("Test File 1.html"),
         //     //     new TabbedWindow([
@@ -20,11 +30,3 @@ app.on("ready", () =>
         //     // ),
         //     new Window("Test File 3.html")
         // )
-    );
-    layout.updateStyles(styles)
-    layout.setLayout(SplitType.HORIZONTAL, 25, new Window("Test File 3.html"))
-
-    let layoutHtml = layout.GenerateHtml(rootPath);
-
-    mainWindow.loadURL(`data:text/html;charset=utf-8,${layoutHtml}`);
-});
